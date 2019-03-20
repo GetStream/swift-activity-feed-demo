853cbeb13dcc749cc75494ebe1f6787a6f581dc4
Alexey Bukhtin <bukhtin@gmail.com>
2019-03-20 15:30:03 +0100

    Created the project with Podfile.

---
+++ GetStreamActivityFeedDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GetStreamActivityFeedDemo
+//
+//  Created by Alexey Bukhtin on 20/03/2019.
+//  Copyright © 2019 Stream.io Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,7 @@
+platform :ios, '11.0'
+inhibit_all_warnings!
+
+target 'GetStreamActivityFeedDemo' do
+  use_frameworks!
+  pod 'GetStreamActivityFeed'
+end
