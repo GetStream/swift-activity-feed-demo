ce4dba5dc72dbc2295ea5d36c410ca6e325606b7
Alexey Bukhtin <bukhtin@gmail.com>
2019-03-20 16:19:50 +0100

    Show Detail screen with comments.

--- GetStreamActivityFeedDemo/ViewController.swift
+++ GetStreamActivityFeedDemo/ViewController.swift
@@ -17,9 +17,26 @@ class ViewController: FlatFeedViewController<GetStreamActivityFeed.Activity> {
         if let feedId = FeedId(feedSlug: "timeline") {
             let timelineFlatFeed = Client.shared.flatFeed(feedId)
             presenter = FlatFeedPresenter<GetStreamActivityFeed.Activity>(flatFeed: timelineFlatFeed,
-                                                                          reactionTypes: .likes)
+                                                                          reactionTypes: [.likes, .comments])
         }
         
         super.viewDidLoad()
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if indexPath.row == 0 {
+            // Create a detail view controller.
+            let detailViewController = DetailViewController<GetStreamActivityFeed.Activity>()
+            // Set the activity presenter from the selected cell.
+            detailViewController.activityPresenter = activityPresenter(in: indexPath.section)
+            // Set sections we want to show the activity itself and comments.
+            detailViewController.sections = [.activity, .comments]
+            // Present the detail view controller with UINavigationController
+            // to use the navigation bar to return back.
+            present(UINavigationController(rootViewController: detailViewController), animated: true)
+        } else {
+            // Keep the default behaviour for over rows in the table view.
+            super.tableView(tableView, didSelectRowAt: indexPath)
+        }
     }
 }
