dc079d76b516403e111aed7149e37ce891035cbb
Bahadir Oncel <bahadir.onc@gmail.com>
2020-01-27 16:12:34 +0100

    Update dependencies and refactor
    Updates GetStream and GetStreamActivityFeed dependencies to their latest version, replaces demo api key, id and token with placeholders

--- GetStreamActivityFeedDemo/AppDelegate.swift
+++ GetStreamActivityFeedDemo/AppDelegate.swift
@@ -17,16 +17,19 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Setup Stream Client.
-        Client.config = .init(apiKey: "2ryhd5wu24jk",
-                              appId: "45210",
-                              token: "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoidXNlci1vbmUifQ.-GBDqPI-6P1_x_9CRnw_URZUHaQ-2_M5fTHRKdflsR0")
+        Client.config = .init(apiKey: "<#API_KEY#>",
+                              appId: "<#APP_ID#>",
+                              logsEnabled: true)
         
-        // Setup Stream user.
-        Client.shared.getCurrentUser(typeOf: GetStreamActivityFeed.User.self) { result in
-            // Current user is ready. Load timeline feed.
-            if result.error == nil, let viewController = self.window?.rootViewController as? ViewController {
-                viewController.reloadData()
-            }
+        Client.shared.setupUser(GetStreamActivityFeed.User(name: "<#USER_NAME#>",
+                                                           id: "<#USER_ID#>"),
+                                token: "<#USER_TOKEN#>") { (result) in
+                                    guard result.error == nil else {
+                                        preconditionFailure("Cannot setup user: \(result.error!)")
+                                    }
+                                    if let viewController = self.window?.rootViewController as? ViewController {
+                                        viewController.reloadData()
+                                    }
         }
         
         return true
--- Podfile
+++ Podfile
@@ -3,5 +3,6 @@ inhibit_all_warnings!
 
 target 'GetStreamActivityFeedDemo' do
   use_frameworks!
+  pod 'GetStream'
   pod 'GetStreamActivityFeed'
 end
